f0f456a194cf5fe3acb1bf12d1741b6c307cf2f5
Agni Patel <77357349+patelagni21@users.noreply.github.com>
2021-02-25 10:12:47 -0500

    Add files via upload

---
+++ ordercheck.java
@@ -0,0 +1,15 @@
+public class Ordered
+{
+   public static void main(String[] args)
+   {
+       int w = Integer.parseInt(args[0]);
+       int x = Integer.parseInt(args[1]);
+       int y = Integer.parseInt(args[2]);
+       int z = Integer.parseInt(args[3]);
+      
+       boolean b = (w < x && x < y && y < z) || (w > x && x > y && y > z);
+      
+       System.out.println(b);  
+      
+   }
+}
